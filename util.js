1c8b8da80463538051d29d39caa2150df99a5398
challaSKR <95953904+challaSKR@users.noreply.github.com>
2022-06-15 09:46:58 +0530

    Update util.js

--- util.js
+++ util.js
@@ -1,7 +1,7 @@
 export const createNewDataSource(plantData, relatedSinglePlantRecRop,relatedUserDefinedData,relatedRecommended) {
     const newDataSource = defaultData(plantData);
     _.set(newDataSource[0], 'current', plantData?.CC_REC_ROP);
-    _.set(newDataSource[0], 'userDefined', relatedUserDefined?.)
+    _.set(newDataSource[0], 'userDefined', relatedUserDefined?.inputRop)
     newDataSource.push({
       current : plantData?.REC_SL,
       key:"RecommendedSL",
