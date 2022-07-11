8035df242fe095d9ebfdb8b463d435cc37cbf0c5
challaSKR <95953904+challaSKR@users.noreply.github.com>
2022-07-11 11:26:45 +0530

    Update bulk_approval_create_change_log.sql

--- bulk_approval_create_change_log.sql
+++ bulk_approval_create_change_log.sql
@@ -1,14 +1,13 @@
 CREATE PROCEDURE [dbo].[Bulk_Approval_Create_Changelog]
 (
-  	@bulk_approval_create_changelog bulkApprovalChangeLogType
+  	@bulk_approval_create_changelog bulkApprovalChangeLogType READONLY
 )
   AS
   BEGIN
-  	DECLARE bulk_approval_changelog_cursor CURSOR;
-    
+  	DECLARE bulk_approval_changelog_cursor CURSOR
     FOR SELECT PLANT_ID,MATERIAL_ID,ERP,MRPArea,CHANGED_BY_ID,CHANGED_BY_EMAIL,
     ROP_OLD,ROP_NEW,MAX_OLD,MAX_NEW,MLS_OLD,MLS_NEW,ROUNDING_VALUE_OLD,ROUNDING_VALUE_NEW,SERVICE_LEVEL_NEW
-    BASE_UNIT_OF_MEASURE, COMMENT, MI, MI_IND, CURRENCY_CD, UNIT_COST, UNRISTRICRTED_STOCK_QUANTITY FROM @bulk_approval_changelog
+    BASE_UNIT_OF_MEASURE, COMMENT, MI, MI_IND, CURRENCY_CD, UNIT_COST, UNRISTRICRTED_STOCK_QUANTITY FROM @bulk_approval_changelog;
     
     OPEN bulk_approval_changelog_cursor;
     
