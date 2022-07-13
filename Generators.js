883586db3bb5be4b8b6bb0eae8f427d5de20e8b2
challaSKR <95953904+challaSKR@users.noreply.github.com>
2022-07-13 11:37:39 +0530

    Update Generators.js

--- Generators.js
+++ Generators.js
@@ -68,21 +68,21 @@ export const createIOTAPayload = (plant, material, dataSource, user,data) => {
   return changeObject;
 };
 
-export const createIOTAPayloadForBulkApproval = (plant, material, dataSource,data) => {
+export const createIOTAPayloadForBulkAPI = (plant, material, dataSource,data) => {
       const changeObject = {
         [dataSource[0].key+"_OLD"]: isVal(dataSource[0].current),
-        [dataSource[0].key+"_NEW"]: isVal(dataSource[0].userDefined),
+        [dataSource[0].key+"_NEW"]: isVal(dataSource[0].recommended),
         [dataSource[1].key+"_OLD"]: isVal(dataSource[1].current),
-        [dataSource[1].key+"_NEW"]: isVal(dataSource[1].userDefined),
+        [dataSource[1].key+"_NEW"]: isVal(dataSource[1].recommended),
         [dataSource[2].key+"_OLD"]: isVal(dataSource[2].current),
-        [dataSource[2].key+"_NEW"]: isVal(dataSource[2].userDefined),
+        [dataSource[2].key+"_NEW"]: isVal(dataSource[2].recommended),
         [dataSource[3].key+"_OLD"]: isValue(dataSource[3].current),
-        [dataSource[3].key+"_NEW"]: isValue(dataSource[3].userDefined),
-        [dataSource[4].key+"_NEW"]: dataSource[4].userDefined,
+        [dataSource[3].key+"_NEW"]: isValue(dataSource[3].recommended),
+        [dataSource[4].key+"_NEW"]: dataSource[4].recommended,
         ["BASE_UNIT_OF_MEASURE_SAP_ID"]:data?.BASE_UNIT_OF_MEASURE_SAP_ID,
         ["COMMENTS"]:dataSource["comments"],
-        ["MONITORY_IMPACT"]: (parseFloat(isNumeric(dataSource[0].current))-parseFloat(isNumeric(dataSource[0].userDefined)))*parseFloat(isNumeric(data?.CC_UNIT_COST_AMT)),
-        ["MONITORY_IMPACT_INDICATOR"] :(parseFloat(isNumeric(dataSource[0].current))-parseFloat(isNumeric(dataSource[0].userDefined)))*parseFloat(isNumeric(data?.CC_UNIT_COST_AMT)) <0 ? "Investment":(parseFloat(isNumeric(dataSource[0].current))-parseFloat(isNumeric(dataSource[0].userDefined)))*parseFloat(isNumeric(data?.CC_UNIT_COST_AMT)) >0 ? "Reduction" :"No Impact",
+        ["MONITORY_IMPACT"]: (parseFloat(isNumeric(dataSource[0].current))-parseFloat(isNumeric(dataSource[0].recommended)))*parseFloat(isNumeric(data?.CC_UNIT_COST_AMT)),
+        ["MONITORY_IMPACT_INDICATOR"] :(parseFloat(isNumeric(dataSource[0].current))-parseFloat(isNumeric(dataSource[0].recommended)))*parseFloat(isNumeric(data?.CC_UNIT_COST_AMT)) <0 ? "Investment":(parseFloat(isNumeric(dataSource[0].current))-parseFloat(isNumeric(dataSource[0].recommended)))*parseFloat(isNumeric(data?.CC_UNIT_COST_AMT)) >0 ? "Reduction" :"No Impact",
         ["CURRENCY"]:data?.COMPANY_CURRENCY_SAP_CD,
         ["UNIT_COST"]:data?.CC_UNIT_COST_AMT,
         ["TOTAL_STOCK"]:isValue(data?.EDAM_VALUATED_UNRESTRICTED_USE_STOCK_QTY),
